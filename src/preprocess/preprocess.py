fdb2bdbfad4c35cbd250ba1958ea5a7b07a06414
Christos Vrachas <cvracdi@gmail.com>
2019-02-11 18:52:03 +0200

    light preprocessor: need to add functionality

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -1,7 +1,36 @@
-from nltk.stem.snowball import SnowballStemmer
+#from nltk.stem.snowball import SnowballStemmer
+
+
+"""
+    LightPreprocessor class is the base class for preprocessing the input texts
+
+    TODO: describe LightPreprocessor functionality
+"""
 
 class LightPreprocessor:
 
-    def __init__(self, input_text_set):
+    def __init__(self, input_text_set=None, input_df=None, classes=None):
         self.input_text_set = input_text_set
-        self.train_df = pd.read_csv(self.input_text_set)
+
+        if self.input_text_set:
+            self.df = pd.read_csv(self.input_text_set)
+        else:
+            self.df = input_df
+            self.classes = classes
+        self.content_per_cat = self._text_per_category()
+
+    def _text_preprocess(self, text):
+        return None
+
+    # Joins all articles of a given category into a single string
+    def _join_text(self, label):
+        return ' '.join(self.df.loc[self.df['Category'] == label]['Content'].values)
+
+    # Populates single string content per category
+    def _text_per_category(self):
+        return {category: self._join_text(category) for category in self.classes}
+
+    # Given a category, return its joined article content
+    def preprocessed_text_per_cat(self, category):
+        return self.content_per_cat[category]
+
--- src/word_cloud/word_cloud.py
+++ src/word_cloud/word_cloud.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from sklearn import preprocessing
 from wordcloud import WordCloud
+from preprocess.preprocess import *
 
 class WordCloudGen:
 
@@ -16,14 +17,15 @@ class WordCloudGen:
         label_encoder.fit(self.train_df['Category'])
 
         self.classes = label_encoder.classes_
+        self.preprocessor = LightPreprocessor(input_df=self.train_df, classes=self.classes)
 
     # Builds WordClouds - one per class
     def generate_wordclouds(self):
 
-        for label in self.classes : 
-            text = self.train_df.loc[self.train_df['Category'] == label]['Content'].values
+        for label in self.classes:
+            text = self.preprocessor.preprocessed_text_per_cat(label)
             wordcloud = WordCloud(max_words=1000,max_font_size=40, margin=10,
-                    random_state=1, width=840, height=420).generate((np.array2string(text)))
+                        random_state=1, width=840, height=420).generate(text)
             wordcloud.to_file(self.path + label + '_wordcloud.png')
 
 
